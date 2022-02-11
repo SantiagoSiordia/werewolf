e73a91f9a6d2a1e8e5466ec908311d8dfb2a3db7
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-02-10 22:57:51 -0600

    feat: types now declared

--- src/services/cloud-firestore/cloud-firestore.ts
+++ src/services/cloud-firestore/cloud-firestore.ts
@@ -1,15 +1,5 @@
 import firestore from '@react-native-firebase/firestore';
 
-export interface Player {
-    name: string;
-    role: string;
-}
-
-export interface Game {
-    moderator: string;
-    players: Array<Player>
-}
-
 export const getGame = async (gameId: string) => {
     try {
         const response = await firestore().collection('games').doc(gameId).get();

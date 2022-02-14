41c01a8fda4e325ac07f30610474ca43784a7559
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-02-14 13:57:25 -0600

    feat: change language added in landing

--- src/features/LandingScreen/LandingScreen.tsx
+++ src/features/LandingScreen/LandingScreen.tsx
@@ -2,20 +2,37 @@ import { WwButton } from "@components";
 import { useAppTranslation } from "@features";
 import React, { Dispatch, FC, SetStateAction } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export type LandingScreenProps = {
     setIsLanding: Dispatch<SetStateAction<boolean>>
 }
 
+const newLanguage: Record<string, "en-US" | "es-MX"> = {
+    "en-US": "es-MX",
+    "es-MX": "en-US"
+}
+
 export const LandingScreen: FC<LandingScreenProps> = ({ setIsLanding }) => {
 
-    const { t } = useAppTranslation()
+    const { t, i18n } = useAppTranslation()
 
-    return <View style={styles.container}>
+    const handleOnChangeLanguage = () => i18n.changeLanguage(newLanguage[i18n.language])
+
+    return <SafeAreaView style={styles.safeAreaBackgrounColor}>
+        <View style={styles.container}>
         <Text style={styles.welcome}>{t("landing.welcome to")}</Text>
         <Text style={styles.werewolf}>{t("app.title")}</Text>
         <WwButton text={t("general purpose.continue")} onPress={() => setIsLanding(false)} icon />
+        <View style={styles.languageButtonContainer}>
+            <WwButton
+                text={t("general purpose.change language")}
+                variant="blue"
+                onPress={handleOnChangeLanguage}
+            />
+        </View>
     </View>
+    </SafeAreaView>
 }
 
 const styles = StyleSheet.create({
@@ -36,5 +53,12 @@ const styles = StyleSheet.create({
         fontSize: 48,
         color: "white",
         textTransform: "uppercase"
+    },
+    safeAreaBackgrounColor: {
+        backgroundColor: "black"
+    },
+    languageButtonContainer: {
+        position: "absolute",
+        bottom: 16
     }
 })

6c1e4fdaf62c39b87112891b992fe045bd01c98d
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2022-03-09 20:46:16 -0600

    deletion: unused draggable component

--- src/features/SettingsScreen/components/index.ts
+++ src/features/SettingsScreen/components/index.ts
@@ -1,6 +1,6 @@
-export * from "./Draggable"
 export * from "./GoCreateNewGame"
 export * from "./PlayerInput"
+export * from "./SelectedRoleQuantity"
 export * from "./Settings"
 export * from "./SettingsLogic"
-export * from "./SelectedRoleQuantity"
+
